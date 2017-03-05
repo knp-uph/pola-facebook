b217bb890e3302a61584b00bf5f11b6aaf489e73
Jakub Malinowski <jaqb_malinowski@yahoo.com>
2017-03-05 18:15:17 +0100

    init commit, create project

---
+++ src/main/java/com/polafacebook/PolaFacebookApplication.java
@@ -0,0 +1,12 @@
+package com.polafacebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolaFacebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolaFacebookApplication.class, args);
+	}
+}
