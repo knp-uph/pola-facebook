e5a901da3db565032c837e3f4d2d3c431c6aa027
Plotron <piotr.graniszewski@op.pl>
2019-02-13 08:29:46 +0100

    Refactoring, finishing touches

--- src/main/java/com/polafacebook/ports/outgoing/OnNewOutgoingMessageListener.java
+++
@@ -1,7 +0,0 @@
-package com.polafacebook.ports.outgoing;
-
-import com.polafacebook.process.engine.message.OutgoingMessage;
-
-public interface OnNewOutgoingMessageListener {
-    void onNewMessage(OutgoingMessage message);
-}
